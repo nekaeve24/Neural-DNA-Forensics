22ecb045feb05ccdd465ad789f330a2f4600ca76
Neka Everett <neka.everett@gmail.com>
2026-02-01 17:53:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def is_within_office_hours(dt):
 
 # --- 1. THE SOVEREIGN VAULT (POSTGRESQL) ---
 # Hardwired Local Link - Try this first
-DATABASE_URL = "postgresql://postgres:aKDmjJPDwCCLqAGxdJfGXFHTSRbKrmkQ@caboose.proxy.rlwy.net:34965/railway"
+DATABASE_URL = "postgresql://postgres:aKDmjJPDwCCLqAGxdJfGXFHTSRbKrmkQ@postgres.railway.internal:5432/railway"
 
 def get_db_connection():
     """Safety Valve: Prevents the app from crashing if the Vault is offline"""
